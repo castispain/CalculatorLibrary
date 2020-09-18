8187cae870ca37b13d71f1a29d07e70143f57da8
Jose Luis Serrano Castillejo <JoseLuis.Serrano@edf-re.com>
2020-09-18 16:19:33 -0700

    division

--- calculator.py
+++ calculator.py
@@ -9,3 +9,10 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+def multiply(first_term, second_term):
+    return first_term * second_term
+
+def division(first_term, second_term):
+    return first_term/float(second_term)
